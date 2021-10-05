168a38e9fc15bbeb1fa4cc9592da395ae358200f
Maitre Gims <lionel.kingue@yahoo.fr>
2021-10-05 16:31:08 +0100

    Changes routes Files

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact' , [ContactController::class, 'Contact'])->name('contact');
